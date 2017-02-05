623bc1d1f53553900d9c19316038b12fb9923faf
zhufyakvv <zhufyakvv@gmail.com>
2017-02-05 19:16:13 +0200

    Cache cfg in settings

--- UsingDataWizAPI/settings.py
+++ UsingDataWizAPI/settings.py
@@ -126,7 +126,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': '/var/tmp/django_cache',
-        'TIMEOUT': 60,
         'OPTIONS': {
             'MAX_ENTRIES': 1000
         }
