0a05d5a01e4f400521f1ba3e0dff738129d370b9
zhufyakvv <zhufyakvv@gmail.com>
2017-02-06 16:49:55 +0200

    css heroku static specific

--- UsingDataWizAPI/urls.py
+++ UsingDataWizAPI/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import patterns
 from django.conf.urls import url, include
 from django.contrib import admin
-from patterns import patterns
 
 from UsingDataWizAPI import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
