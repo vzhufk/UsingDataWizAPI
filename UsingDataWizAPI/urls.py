f5fa07780240f766580d9003b4ec7250559d11fe
zhufyakvv <zhufyakvv@gmail.com>
2017-02-06 17:10:46 +0200

    css

--- UsingDataWizAPI/urls.py
+++ UsingDataWizAPI/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -24,6 +23,3 @@ urlpatterns = [
     url(r'^', include('application.urls')),
 ]
 
-urlpatterns += patterns('',
- (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
- )
